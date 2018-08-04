f16d9656c156449427c5fefab67d3bcfeba04580
Afrid1719 <30635201+Afrid1719@users.noreply.github.com>
2018-08-04 17:24:54 +0530

    Made changes in the iframe

--- Fixtures.js
+++ Fixtures.js
@@ -3,11 +3,16 @@ function show_video(){
     
     var frame=document.getElementById("video-frame");
     frame.style.display="block";
-    frame.style.animation="video-pop 1s ease";
     frame.style.width="100%";
     frame.style.height="60%";
     var vid_src="https://www.youtube.com/embed/cqnotjll3mg"; //source for the video
     document.getElementById('vid').style.display="block";
-    document.getElementById('vid').src=vid_src; //will add the source after the animation
-    
+    document.getElementById('vid').src=vid_src; //it will add the source 
+    document.getElementById("main-container").style.opacity="0.45";
+
+}
+
+function close_frame(){
+    document.getElementById('video-frame').style.display="none";
+    document.getElementById("main-container").style.opacity="1";
 }
