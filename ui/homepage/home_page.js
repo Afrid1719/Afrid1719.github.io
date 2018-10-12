d81127a8269df663af5f9ca10f6fbd05edf12386
Afrid1719 <30635201+Afrid1719@users.noreply.github.com>
2018-10-12 17:32:06 +0530

    New Homepage v1.0

--- ui/homepage/home_page.js
+++ ui/homepage/home_page.js
@@ -3,18 +3,21 @@ function home()
     document.getElementById("Tournaments").style.display='none';
     document.getElementById("Contact").style.display='none';
     document.getElementById("Home").style.display='block';
+    document.getElementById("pic-container").style.display='flex';
 }
 
 function tournaments()
 {
     document.getElementById("Contact").style.display='none';
     document.getElementById("Home").style.display='none';
+    document.getElementById("pic-container").style.display='none';
     document.getElementById("Tournaments").style.display='block';
 }
 
 function contact()
 {
     document.getElementById("Home").style.display='none';
+    document.getElementById("pic-container").style.display='none';
     document.getElementById("Tournaments").style.display='none';
     document.getElementById("Contact").style.display='block';
 }
